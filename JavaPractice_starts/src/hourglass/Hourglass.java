331a43dbb02f4786d58598c3ff36a733eafa1bc8
NaonLee <giraffe1254@gmail.com>
2021-01-02 19:41:08 +0900

    Hourglass

--- JavaPractice_starts/src/hourglass/Hourglass.java
+++ JavaPractice_starts/src/hourglass/Hourglass.java
@@ -1,11 +1,48 @@
 package hourglass;
 
+import java.util.Scanner;
+
 //hourglass using the middle number
 public class Hourglass {
 
 	public static void main(String[] args) {
 		
-
+		
+	}
+	
+	public static void hourglass_1() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the height of hourglass: ");
+		
+		int height = sc.nextInt(); //height
+		int middle = height/2+1;	//middle number
+		 System.out.println();
+		 
+		for(int i = 1; i <= height; i++) {
+			if(i <= middle) {		//if row <= middle number
+				for(int j = 1; j <= height-i+1; j++) {
+					if(i > j)
+						System.out.print(" ");
+					else
+						System.out.print("*");
+				}
+			}
+			else {
+				for(int j = 1; j <= i; j++) {
+					if(height-i >= j) 
+						System.out.print(" ");
+					else
+						System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void hourglass_2() {
+	
+		
 	}
 
 }
